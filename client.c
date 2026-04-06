9bce11f870161abbc474ccf9658473302722d288
EZ121906 <edric09821@gmail.com>
2026-04-05 21:59:52 -0400

    Update client.c
    implemented the gradient calculating portion of client.c

--- client.c
+++ client.c
@@ -33,6 +33,19 @@ int write_all(int fd, const void *buf, size_t count) {
     return 1;
 }
 
+model_t computer_gradient(HouseData *data, int n, model_t current) {
+    model_t grad = {0.0, 0.0}
+
+    for(int i = 0; i < n; i++_{
+        float prediction = current.weight * data[i].sqft + current.bias;
+        float error = prediction - data[i].price;
+        grad.weight += error * data[i].sqft;
+        grad.bias += error;
+    }
+    grad.weight /= num_points;
+    grad.bias /= num_points;
+}
+
 int main() {
     int client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (client_socket == -1) {
@@ -57,11 +70,29 @@ int main() {
 
     printf("Connected to server\n");
 
+    int num_points;
+    int read_status = read_all(sock, &num_points, sizeof(int));
+    if (read_status == 0) {
+        printf("Server disconnected.\n");
+        break;
+    } else if (read_status == -1) {
+        perror("read_all failed");
+        break;
+    }
+    
+    house_data_t *my_shard = malloc(sizeof(house_data_t) * num_points);
+    read_status = read_all(sock, my_shard, sizeof(house_data_t) * num_points);
+    if (read_status == 0) {
+        printf("Server disconnected.\n");
+        break;
+    } else if (read_status == -1) {
+        perror("read_all failed");
+        break;
+    }
+    
+    printf("Worker ready. Received shard of %d points.\n", num_points);
+
     model_t server_model;
-    model_t hardcoded_model = {
-        .weight = 1.0,
-        .bias = 0.0,
-    };
     
     while (1) {
         int read_status = read_all(client_socket, &server_model, sizeof(model_t));
@@ -74,14 +105,18 @@ int main() {
             break;
         }
 
-        int write_status = write_all(client_socket, &hardcoded_model, sizeof(model_t));
+        model_t gradient = compute_gradient(my_shard, num_points, current_model)
+
+        int write_status = write_all(client_socket, &gradient, sizeof(model_t));
         
         if (write_status <= 0) {
             perror("write_all failed or disconnected");
             break;
         }
     }
-
+    
+    printf("Training complete. Shutting down worker.\n");
+    free(my_shard);
     close(client_socket);
     return 0;
 }
